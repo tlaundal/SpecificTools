3b4b8699ecbaff06cb0e5ec7aec0b6a08e92dc8c
totokaka <ttotokaka@gmail.com>
2012-08-21 13:51:23 +0200

    The parsing part of Replacements is updated for MultiWorld

--- src/main/java/me/totokaka/specifictools/Replacements.java
+++ src/main/java/me/totokaka/specifictools/Replacements.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
@@ -13,33 +14,44 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class Replacements {
 	
-	Map<Material, Set<Material>> replacements = new HashMap<Material, Set<Material>>();
+	Map<String, Map<Material, Set<Material>>> replacements = new HashMap<String, Map<Material, Set<Material>>>();
 	
 	public void load(SpecificTools plugin){
 		FileConfiguration config = plugin.getConfig();
 		ConfigurationSection section = config.getConfigurationSection("Replacements");
-		Set<String> keys = section.getKeys(false);
-		if(keys != null){
-			for(String s : keys){
-				Material m = Material.getMaterial(s);
-				if(m != null){
-					@SuppressWarnings("unchecked")
-					List<String> list = (List<String>) section.getList(s);
-					Set<Material> tools = new HashSet<Material>();
-					for(String tool : list){
-						if(tool.equals("HAND")){
-							tools.add(Material.AIR);
-						}else if(Material.valueOf(tool) != null){
-							tools.add(Material.valueOf(tool));
+		Set<String> wKeys = section.getKeys(false);// the worlds
+		if(wKeys != null){
+			for(String s : wKeys){
+				Set<String> keys = section.getConfigurationSection(s).getKeys(false);// the blocks
+				if(keys != null || Bukkit.getWorld(s) != null){
+					for(String material : keys){
+						Material m = Material.getMaterial(material);
+						if(m != null){
+							@SuppressWarnings("unchecked")
+							List<String> list = (List<String>) section.getList(s+"."+material);
+							Set<Material> tools = new HashSet<Material>();
+							for(String tool : list){
+								if(tool.equals("HAND")){
+									tools.add(Material.AIR);
+								}else if(Material.valueOf(tool) != null){
+									tools.add(Material.valueOf(tool));
+								}else{
+									plugin.getLogger().warning("Looks like there is something wrong with your config.yml. "
+											 +tool+" is not a valid tool!");
+								}
+							}
+							if(replacements.get(s) != null){
+								replacements.put(s, new HashMap<Material, Set<Material>>());
+							}
+							replacements.get(s).put(m, tools);
 						}else{
 							plugin.getLogger().warning("Looks like there is something wrong with your config.yml. "
-									 +tool+" is not a valid tool!");
+									+material+" is not a valid block!");
 						}
 					}
-					replacements.put(m, tools);
 				}else{
 					plugin.getLogger().warning("Looks like there is something wrong with your config.yml. "
-							+s+" is not a valid block!");
+							+s+" is not a world or has no children!");
 				}
 			}
 		}
