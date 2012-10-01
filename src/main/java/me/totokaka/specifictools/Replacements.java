16951dc28de8f1c0f47f598cbb59cdb0916f2229
totokaka <ttotokaka@gmail.com>
2012-10-01 12:05:40 +0200

    rewrote parseConfig in Replacements to the new config

--- src/main/java/me/totokaka/specifictools/Replacements.java
+++ src/main/java/me/totokaka/specifictools/Replacements.java
@@ -13,25 +13,20 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class Replacements {
-	
-	Map<String, Map<Material, Set<Material>>> replacements;
+	//worlds         blocks            tools         actions
+	HashMap<String, HashMap<String, HashMap<String, List<String>>>> replacements;
 	SpecificTools plugin;
 	
 	public Replacements(final SpecificTools plugin){
 		this.plugin = plugin;
 	}
 	
-	public Set<Material> getToolsByBlock(final Material m, final World world) {
-		if (replacements.containsKey(world.getName())) {
-			if (replacements.get(world.getName()).containsKey(m)) {
-				return replacements.get(world.getName()).get(m);
-			}
-		}
+	public List<String> getToolsByBlock(final Material m, final World world) {
 		return null;
 	}
 	
 	public boolean load() {
-		replacements = new HashMap<String, Map<Material, Set<Material>>>();
+		replacements = new HashMap<String, HashMap<String, HashMap<String, List<String>>>>();
 		try {
 			parseConfig(plugin.getNewConfig().getConfig());
 		} catch (final Exception ex) {
@@ -49,49 +44,51 @@ public class Replacements {
 	public void parseConfig(final FileConfiguration config) {
 		final ConfigurationSection section = config
 				.getConfigurationSection("Replacements");
-		final Set<String> wKeys = section.getKeys(false);// the worlds
-		if (wKeys != null) {
-			for (final String s : wKeys) {
-				final Set<String> keys = section.getConfigurationSection(s)
-						.getKeys(false);// the blocks
-				if (keys != null || Bukkit.getWorld(s) != null) {
-					for (final String material : keys) {
+		final Set<String> worlds = section.getKeys(false);// the worlds
+		if (worlds != null) {
+			for (final String world : worlds) {
+				final Set<String> blocks = section.getConfigurationSection(world).getKeys(false);
+				if(blocks != null && Bukkit.getWorld(world) != null){
+					for(final String material : blocks){
 						final Material m = Material.getMaterial(material);
 						if (m != null && m.isBlock()) {
-							@SuppressWarnings("unchecked")
-							final List<String> list = (List<String>) section
-									.getList(s + "." + material);
-							final Set<Material> tools = new HashSet<Material>();
-							for (String tool : list) {
-								if (tool.equals("HAND")) {
-									tools.add(Material.AIR);
-								} else if (Material.valueOf(tool) != null) {
-									tools.add(Material.valueOf(tool));
-								} else {
-									plugin.getLogger().warning(
-											"Looks like there is something wrong with your config.yml. "
-													+ tool
-													+ " is not a valid tool!");
+							Set<String> tools = config.getConfigurationSection(world).getConfigurationSection(material).getKeys(false);
+							HashMap<String, List<String>> toolmap = new HashMap<String, List<String>>();
+							for(String tool : tools){
+								//replacements.get(world).get(material).put(tool, config.getConfigurationSection(world).getConfigurationSection(material).getStringList(tool));
+								if(!(tool.equals("HAND") || Material.getMaterial(tool) != null)){
+									plugin.getLogger().warning(tool+" Is not a tool!! the plugin will probably fuck up!! please stop the server and fix.");
 								}
+								List<String> actions = config.getConfigurationSection(world).getConfigurationSection(material).getStringList(tool);
+								for(String action : actions){
+									if(!(action.equals("drop") || action.equals("destroy"))){
+										plugin.getLogger().warning(action+" Is not an action!! the plugin will probably fuck up!! please stop the server and fix.");
+									}
+								}
+								toolmap.put(tool, actions);
 							}
-							if (replacements.get(s) == null) {
-								replacements.put(s,
-										new HashMap<Material, Set<Material>>());
+							//worlds
+							if(replacements.get(world) == null){
+								replacements.put(world, new HashMap<String, HashMap<String,List<String>>>());
 							}
-							replacements.get(s).put(m, tools);
+							//blocks
+							if(replacements.get(world).get(material) == null){
+								replacements.get(world).put(material, new HashMap<String, List<String>>());
+							}
+							replacements.get(world).get(material).putAll(toolmap);
 						} else {
 							plugin.getLogger().warning(
 									"Looks like there is something wrong with your config.yml. "
-											+ material
-											+ " is not a valid block!");
+									+ material
+									+ " is not a valid block!");
 						}
 					}
 				} else {
 					plugin.getLogger()
-							.warning(
-									"Looks like there is something wrong with your config.yml. "
-											+ s
-											+ " is not a world or has no children!");
+						.warning(
+								"Looks like there is something wrong with your config.yml. "
+								+ world
+								+ " is not a world or has no children!");
 				}
 			}
 		}
